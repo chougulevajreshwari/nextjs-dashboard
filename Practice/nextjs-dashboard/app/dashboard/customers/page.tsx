611860d42535049658bc6154b8e0b74842632638
chougulevajreshwari <v.chougule@zensar.com>
2025-03-08 11:32:55 +0530

    NextJs-Dashboard

---
+++ Practice/nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+const Customer = ()=>{
+    return<h1>This is Customers page</h1>
+}
+export default Customer;
---
+++ Practice/nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+ const Page = ()=>{
+    return(<h1>This is dashboard component</h1>)
+}
+
+export default Page;
